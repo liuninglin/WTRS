b62ca7f792ef6159fcb6d98f6150d9ba4bf95bd5
NingLin Liu <liuninglin0@msn.com>
2016-02-24 10:22:40 +0800

    更新

---
+++ src/test/java/com/test/ContainerTest.java
@@ -0,0 +1,32 @@
+/**
+* Copyright ? 2016-2-23 liuninglin
+* WTRS 上午10:40:39
+* Version 1.0
+* All right reserved.
+*
+*/
+
+package com.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * 类描述： 
+ * 创建者：刘宁林
+ * 项目名称： WTRS
+ * 创建时间： 2016-2-23 上午10:40:39
+ * 版本号： v1.0
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:configs/spring-context.xml"}) 
+public class ContainerTest
+{
+    @Test
+    public void testAll()
+    {
+        
+    }
+}
--- target/WTRS/login.jsp
+++
@@ -1,4 +0,0 @@
-<%@ page language="java" import="java.util.*" pageEncoding="utf-8"%>
-<script type="text/javascript">
-window.location.href = "loginPage.html";
-</script>
